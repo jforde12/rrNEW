98986522b90d8b5de7682598591027c931a94019
jforde12 <jmforde19@denver.k12.ia.us>
2019-01-10 18:27:46 -0600

    fixed tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/drive/DriveConstants.java
@@ -24,11 +24,11 @@ public class DriveConstants {
 
     public static double WHEEL_RADIUS = 2; // in
     public static double GEAR_RATIO = 1; // output (wheel) speed / input (motor) speed
-    public static double TRACK_WIDTH = 12.86; // in
+    public static double TRACK_WIDTH = 14.05; // in
 
     public static DriveConstraints BASE_CONSTRAINTS = new DriveConstraints(30.0, 30.0, Math.PI / 2, Math.PI / 2);
 
-    public static double kV = .0195;
+    public static double kV = .0175;
     public static double kA = 0;
     public static double kStatic = 0;
 
